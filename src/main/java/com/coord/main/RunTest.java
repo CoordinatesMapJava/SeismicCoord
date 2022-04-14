349c14451c67cd3e1188d0a9e59c6db05b88db03
mariacarolinass <m.carolina.soares1@gmail.com>
2022-04-14 14:53:33 -0300

    add class run test

---
+++ src/main/java/com/coord/main/RunTest.java
@@ -0,0 +1,14 @@
+package com.coord.main;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunTest implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+    
+}
