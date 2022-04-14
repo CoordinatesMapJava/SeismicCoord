40c529abba0dd7948456af6f9fda9f8438a26479
mariacarolinass <m.carolina.soares1@gmail.com>
2022-04-14 11:22:59 -0300

    add project

---
+++ src/test/java/com/coord/SeismicCoord/SeismicCoordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coord.SeismicCoord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeismicCoordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
