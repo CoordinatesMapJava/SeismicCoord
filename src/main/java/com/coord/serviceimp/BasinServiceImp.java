354f3099521d39d12dba6b5398abf199a59a783c
mariacarolinass <m.carolina.soares1@gmail.com>
2022-04-14 16:54:23 -0300

    add implement methods

--- src/main/java/com/coord/serviceimp/BasinServiceImp.java
+++ src/main/java/com/coord/serviceimp/BasinServiceImp.java
@@ -1,9 +1,21 @@
 package com.coord.serviceimp;
 
+import com.coord.model.Basin;
 import com.coord.service.BasinService;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service(value = "basinRepoService")
 public class BasinServiceImp implements BasinService {
+
+    @Override
+    public List<Basin> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Basin save(Basin save) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
--- src/main/java/com/coord/serviceimp/Seismic2dServiceImp.java
+++ src/main/java/com/coord/serviceimp/Seismic2dServiceImp.java
@@ -1,9 +1,21 @@
 package com.coord.serviceimp;
 
+import com.coord.model.Seismic2d;
 import com.coord.service.Seismic2dService;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service(value = "seismic2dRepoService")
 public class Seismic2dServiceImp implements Seismic2dService {
+
+    @Override
+    public List<Seismic2d> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Seismic2d save(Seismic2d save) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
