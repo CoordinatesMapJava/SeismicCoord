0a114867a2bb32c17808e5ee822afe273081635b
mariacarolinass <m.carolina.soares1@gmail.com>
2022-04-20 15:16:07 -0300

    add method abstract get by seismic2d end coord

--- src/main/java/com/coord/service/ServiceBase.java
+++ src/main/java/com/coord/service/ServiceBase.java
@@ -1,11 +1,12 @@
 package com.coord.service;
 
-import com.coord.model.EntityBase;
+import com.coord.model.*;
 import java.util.List;
 
 public interface ServiceBase {
 
     public abstract List <EntityBase> getAll();
     public abstract EntityBase save (EntityBase s);
+    public abstract List <Coord> getBySeismic2dEndCoord (String name);
 
 }
